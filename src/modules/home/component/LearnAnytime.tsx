e591bdf33041d447ede1831366141ea0bf457349
vickydev03 <vickysinghdev03@gmail.com>
2026-03-09 22:54:31 +0530

    Update LearnAnytime.tsx

--- src/modules/home/component/LearnAnytime.tsx
+++ src/modules/home/component/LearnAnytime.tsx
@@ -1,4 +1,4 @@
--import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import React from "react";
 import Link from "next/link";
