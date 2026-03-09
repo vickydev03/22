7ad49c82e06a2d431b0dd44d4dc7135ed01c20f0
vickydev03 <vickysinghdev03@gmail.com>
2026-03-09 18:51:26 +0530

    Update ProfileContainer.tsx

--- src/modules/home/component/ProfileContainer.tsx
+++ src/modules/home/component/ProfileContainer.tsx
@@ -216,7 +216,7 @@ function ProfileContainer() {
                     </div>
                   </div>
                 );
-                  </div/>
+                  </div>
               })}
             </div>
           </TabsContent>
